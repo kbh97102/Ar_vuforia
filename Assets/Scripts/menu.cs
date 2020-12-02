d49b2d5562bc8d47977fe79c401e28aeb3bd8336
Arakene <kbh97102@naver.com>
2020-12-02 23:20:22 +0900

    Add gitignore

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+
+    public void OnClickButton1()
+    {
+        Debug.Log("Click!");
+    }
+
+}
